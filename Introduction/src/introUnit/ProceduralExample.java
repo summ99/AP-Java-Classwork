bf149a998aeceb6f05c7c7e1066c7ad982bba380
summ99 <jchen5179@bths.edu>
2016-09-15 13:18:50 -0400

    Update ProceduralExample.java

--- Introduction/src/introUnit/ProceduralExample.java
+++ Introduction/src/introUnit/ProceduralExample.java
@@ -9,7 +9,9 @@ package introUnit;
  */
 public class ProceduralExample {
 	public static void main(String[]args){ 
+		String[] years = {"sophomore","junior","senior"};
 		String[] names = {"Jillian","Joseph","Jordan"};
-		System.out.println("My name is "+ names[0] + ".");
+		System.out.println("My name is "+ names[0] + "and I am a "+ years[2] + ".");
+		
 	}
 }
